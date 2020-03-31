79b40b210a604ec08876c656ac483bdc66ac201e
csdoker <csd758371536@qq.com>
2020-03-31 22:02:35 +0800

    动态生成tab-header模块

--- src/Tabs/index.js
+++ src/Tabs/index.js
@@ -3,18 +3,36 @@ import '../../style/tab.scss'
 class Tabs {
   constructor ($container) {
     this.$container = $container
-    this.$$tabItems = $container.querySelectorAll('.tab-item')
     this.$$tabPanels = $container.querySelectorAll('.tab-panel')
-    this.$tabLine = $container.querySelector('.tab-line')
     this.initTabs()
+    this.setTabs()
     this.bindTabs()
   }
+
   initTabs () {
+    const $tabHeader = document.createElement('div')
+    $tabHeader.setAttribute('class', 'tab-header')
+    this.$$tabPanels.forEach($panel => {
+      const $tabItem = document.createElement('span')
+      $tabItem.setAttribute('class', 'tab-item')
+      $tabItem.innerText = $panel.dataset.name
+      $tabHeader.appendChild($tabItem)
+    })
+    const $tabLine = document.createElement('span')
+    $tabLine.setAttribute('class', 'tab-line')
+    $tabHeader.appendChild($tabLine)
+    this.$container.insertBefore($tabHeader, document.querySelector('.tab-content'))
+  }
+
+  setTabs () {
+    this.$$tabItems = this.$container.querySelectorAll('.tab-item')
+    this.$tabLine = this.$container.querySelector('.tab-line')
     const { offsetWidth, offsetLeft } = this.$$tabItems[0]
     this.setTabItem(this.$$tabItems[0])
     this.setTabLine(offsetWidth, offsetLeft)
     this.setTabPanel(this.$$tabItems[0])
   }
+
   bindTabs () {
     this.$$tabItems.forEach($tab => {
       $tab.addEventListener('click', () => {
@@ -24,16 +42,19 @@ class Tabs {
       })
     })
   }
-  setTabItem($tab) {
+
+  setTabItem ($tab) {
     this.$$tabItems.forEach($tab => $tab.classList.remove('active'))
     $tab.classList.add('active')
   }
-  setTabPanel($tab) {
+
+  setTabPanel ($tab) {
     const index = [...this.$$tabItems].indexOf($tab)
     this.$$tabPanels.forEach($panel => $panel.classList.remove('active'))
     this.$$tabPanels[index].classList.add('active')
   }
-  setTabLine(width, left) {
+
+  setTabLine (width, left) {
     this.$tabLine.style.width = `${width}px`
     this.$tabLine.style.transform = `translateX(${left}px)`
   }
