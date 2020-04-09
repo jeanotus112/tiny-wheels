88d3d60c8fdd20bbc39a6d5487d3f73c02efe735
csdoker <csd758371536@qq.com>
2020-04-09 22:38:29 +0800

    修改Carousel组件部分样式细节，添加文档内容

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Tabs from './Tabs'
 import Collapse from './Collapse'
 import Pager from './Pager'
 import Carousel from './Carousel'
-import Tree from '.tree'
+import Tree from './Tree'
 
 export { Tabs, Collapse, Pager, Carousel, Tree }
