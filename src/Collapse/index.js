d1b7ea75aac0a87e3ab35c74702725c46eb319bb
csdoker <csd758371536@qq.com>
2020-04-13 10:13:16 +0800

    重构Collapse组件代码，优化动画效果

--- src/Collapse/index.js
+++ src/Collapse/index.js
@@ -1,5 +1,7 @@
 import '../../style/collapse.scss'
 
+const mergeStyle = ($node, style) => Object.assign($node.style, style)
+
 class Collapse {
   constructor (options) {
     const defaultOptions = {
@@ -23,7 +25,9 @@ class Collapse {
   initCollapsePanels () {
     const $$collapsePanels = this.$container.children
     this.$$collapsePanels = [...$$collapsePanels]
-    this.$$collapsePanels.forEach($panel => $panel.setAttribute('class', 'collapse-panel'))
+    this.$$collapsePanels.forEach($panel =>
+      $panel.setAttribute('class', 'collapse-panel')
+    )
   }
 
   initCollapseItems () {
@@ -47,23 +51,34 @@ class Collapse {
   setCollapse () {
     this.$$collapseItems = this.$container.querySelectorAll('.collapse-item')
     this.setCollapseItem()
+    this.setCollapsePanels()
   }
 
   setCollapseItem () {
     let collapseKeys = this.$container.dataset.collapseActive
-    this.panelsHeight = []
     if (collapseKeys) {
       collapseKeys = collapseKeys.split(',')
       this.$$collapsePanels.forEach(($panel, index) => {
-        this.panelsHeight.push($panel.offsetHeight)
         if (collapseKeys.indexOf($panel.dataset.collapseKey) !== -1) {
           this.$$collapseItems[index].classList.add('active')
-          $panel.style.height = `${$panel.offsetHeight}px`
-        } else {
-          $panel.style.height = '0px'
         }
       })
     }
+  }
+
+  setCollapsePanels () {
+    this.panelsHeight = []
+    this.$$collapsePanels.forEach(($panel, index) => {
+      this.panelsHeight.push($panel.offsetHeight)
+      if (this.$$collapseItems[index].classList.contains('active')) {
+        $panel.style.height = `${$panel.offsetHeight}px`
+      } else {
+        mergeStyle($panel, { height: '0', paddingBottom: '0' })
+      }
+      setTimeout(() => {
+        $panel.classList.add('animate')
+      })
+    })
   }
 
   bindCollapse () {
@@ -87,7 +102,7 @@ class Collapse {
 
   bindCollapsePanels () {
     this.$$collapsePanels.forEach($panel => {
-      $panel.addEventListener('click', (e) => {
+      $panel.addEventListener('click', e => {
         e.stopPropagation()
       })
     })
@@ -97,7 +112,7 @@ class Collapse {
     this.$$collapseItems.forEach(($item, index) => {
       if ($item !== $bindItem) {
         $item.classList.remove('active')
-        this.$$collapsePanels[index].style.height = '0px'
+        mergeStyle(this.$$collapsePanels[index], { height: '0', paddingBottom: '0' })
       }
     })
   }
@@ -105,10 +120,13 @@ class Collapse {
   toggleCollapse ($bindItem, index) {
     if ($bindItem.classList.contains('active')) {
       $bindItem.classList.remove('active')
-      this.$$collapsePanels[index].style.height = '0px'
+      mergeStyle(this.$$collapsePanels[index], { height: '0', paddingBottom: '0' })
     } else {
       $bindItem.classList.add('active')
-      this.$$collapsePanels[index].style.height = `${this.panelsHeight[index]}px`
+      this.$$collapsePanels[index].style = ''
+      this.$$collapsePanels[
+        index
+      ].style.height = `${this.panelsHeight[index]}px`
     }
   }
 
@@ -116,7 +134,9 @@ class Collapse {
     const collapseActiveKeys = []
     this.$$collapseItems.forEach(($item, index) => {
       if ($item.classList.contains('active')) {
-        collapseActiveKeys.push(this.$$collapsePanels[index].dataset.collapseKey)
+        collapseActiveKeys.push(
+          this.$$collapsePanels[index].dataset.collapseKey
+        )
       }
     })
     return collapseActiveKeys
